25f3edf94debb8ee0b5b4da61a38d6af100a29c4
YY <troyboychoke@mailpluss.com>
2023-06-15 20:47:01 +0330

    change animation with text scroll collapsing

--- app/src/main/java/com/example/compose_mvvm2/ui/detail/materialDetails.kt
+++ app/src/main/java/com/example/compose_mvvm2/ui/detail/materialDetails.kt
@@ -9,63 +9,100 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
+import androidx.compose.ui.unit.min
 import coil.compose.AsyncImage
 import com.example.compose_mvvm2.model.response.mealzModel
 import com.example.compose_mvvm2.ui.theme.Purple200
+import kotlin.math.min
 
 
 @Composable
 fun materialDetails(meal: mealzModel?) {
 
-    var profileState by remember{ mutableStateOf(profileAnimation.NORMAL)}
-    var transition= updateTransition(targetState = profileState,"")
-    val imageSizeDp by transition.animateDp(targetValueByState = {it.dp}, label = "")
-    val color by transition.animateColor(targetValueByState = {it.colorStorke}, label = "")
-    val width by transition.animateDp(targetValueByState = {it.width}, label = "")
+    var scrollState = rememberScrollState()
+    val offSet= min(1f,1-(scrollState.value/600f))
+    val size by animateDpAsState(targetValue = max(100.dp,200.dp*offSet))
+
+//    var profileState by remember{ mutableStateOf(profileAnimation.NORMAL)}
+//    var transition= updateTransition(targetState = profileState,"")
+//    val imageSizeDp by transition.animateDp(targetValueByState = {it.dp}, label = "")
+//    val color by transition.animateColor(targetValueByState = {it.colorStorke}, label = "")
+//    val width by transition.animateDp(targetValueByState = {it.width}, label = "")
 
 
 //    var isExapndImage by remember { mutableStateOf(false) }
 //    val animationImageExpanding: Dp by animateDpAsState(targetValue = if(isExapndImage)  200.dp else 100.dp)
 
-    Column() {
-        Row() {
-            Card(
-                modifier = Modifier.padding(10.dp),
-                border = BorderStroke(width, color)
-            ) {
+    Surface(
+        color = MaterialTheme.colors.background
+    ) {
+        Column() {
 
-                AsyncImage(
-                    model = meal?.strCategoryThumb,
-                    contentDescription = null,
-                    modifier = Modifier.size(imageSizeDp),
+        Surface(elevation = 4.dp) {
+            Row() {
+                Card(
+                    modifier = Modifier.padding(10.dp),
+                    border = BorderStroke(10.dp, Color.Cyan)
+                ) {
+
+                    AsyncImage(
+                        model = meal?.strCategoryThumb,
+                        contentDescription = null,
+                        modifier = Modifier.size(size),
                     )
+                }
+                Text(meal?.strCategory ?: "")
             }
-            Text(meal?.strCategory ?: "")
         }
 
-        Button(onClick = {
+//            Button(onClick = {
+//
+////            isExapndImage=!isExapndImage
+//
+////           profileState= if(profileState==profileAnimation.NORMAL)
+////               profileAnimation.EXAPND
+////            else
+////                profileAnimation.NORMAL
+//
+//            }) {
+//                Text(text = "change")
+//            }
 
-//            isExapndImage=!isExapndImage
 
-           profileState= if(profileState==profileAnimation.NORMAL)
-               profileAnimation.EXAPND
-            else
-                profileAnimation.NORMAL
 
-        }) {
-            Text(text = "change")
+            Column(modifier = Modifier.verticalScroll(scrollState)) {
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+                Text(text = "This Text", modifier = Modifier.padding(10.dp))
+            }
         }
-
     }
+
+
 
 }
 
